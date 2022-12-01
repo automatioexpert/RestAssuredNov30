f6c3c96cc49a86dc0bc508cc5189c744b9e7c72b
Valmiki Vishwakarma <you@example.com>
2022-12-01 17:56:25 +0530

    POJO commit.

---
+++ RestAssuredNov30/src/main/java/POJOPayload/Employee.java
@@ -0,0 +1,42 @@
+package POJOPayload;
+
+public class Employee {
+
+	private String firstName;
+	private String lastName;
+	private int salary;
+	private boolean expert;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public boolean isExpert() {
+		return expert;
+	}
+
+	public void setExpert(boolean expert) {
+		this.expert = expert;
+	}
+
+}
---
+++ RestAssuredNov30/src/main/java/POJOPayload/UseEmployee.java
@@ -0,0 +1,30 @@
+package POJOPayload;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UseEmployee {
+
+	public static void main(String[] args) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		Employee emp = new Employee();
+		emp.setFirstName("VV");
+		emp.setLastName("Vishwa");
+		emp.setSalary(10000000);
+		emp.setExpert(true);
+
+		String jsonEmployee = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(emp);
+
+		System.out.println(jsonEmployee);
+		
+		/*
+		{
+			  "firstName" : "VV",
+			  "lastName" : "Vishwa",
+			  "salary" : 10000000,
+			  "expert" : true
+			}
+			*/
+	}
+}
